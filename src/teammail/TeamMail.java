dfaee0c6765e8b6de069e49362851d380852de63
Nimrod Holguin <nimrodholguin@gmail.com>
2019-03-19 11:47:23 -0400

    iniciando

---
+++ src/teammail/TeamMail.java
@@ -0,0 +1,17 @@
+
+package teammail;
+
+/**
+ *
+ * @author Team Java
+ */
+public class TeamMail {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
